7f10308f946f196653ad0598113243467c8b9489
Myrnyi, Maksym <Maksym.Myrnyi@airberlin.com>
2016-08-03 08:06:01 +0200

    Move old modules

--- modules/java/manifests/params.pp
+++
@@ -1,5 +0,0 @@
-class java::params {
-
-  $webupd8src = '/etc/apt/sources.list.d/webupd8team.list'
-
-}
--- modules/puppetagent/manifests/params.pp
+++
@@ -1,9 +0,0 @@
-# == Class puppetagent::params
-# Default values for puppetagent
-
-class puppetagent::params {
-  $puppetserver        = 'puppetmaster.magalog.net'
-  $working_environment = 'production'
-  $puppetdbserver      = 'puppetmaster.magalog.net'
-  $puppetdbport        = '8081'
-}
