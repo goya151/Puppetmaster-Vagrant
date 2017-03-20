352bfe8e59cd8a036f2eae58022ee4f5b258fe99
Maksym Myrnyi <mmyrnyi@afdata.de>
2017-03-20 09:06:43 +0100

    wget with quiet mode in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,9 @@ echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
       # Check puppet agent installed
       if [ ! -f '/usr/bin/puppet' ]; then
-        cd /tmp && wget http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb && dpkg -i ./puppetlabs-release-pc1-xenial.deb
+        cd /tmp && wget --quiet http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb && dpkg -i ./puppetlabs-release-pc1-xenial.deb
         apt-get update
-        cd /tmp && wget http://apt.puppetlabs.com/pool/xenial/PC1/p/puppet-agent/puppet-agent_1.8.2-1xenial_amd64.deb && dpkg -i ./puppet-agent_1.8.2-1xenial_amd64.deb
+        cd /tmp && wget --quiet http://apt.puppetlabs.com/pool/xenial/PC1/p/puppet-agent/puppet-agent_1.9.3-1xenial_amd64.deb && dpkg -i ./puppet-agent_1.9.3-1xenial_amd64.deb
         ln -s /opt/puppetlabs/bin/puppet  /usr/bin/puppet
       fi
 
@@ -18,6 +18,6 @@ echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
       VERSION=`/usr/bin/puppet --version`
       if [ ! $VERSION = "4.9.4" ]; then
         apt-get -y purge puppet*
-        cd /tmp && wget http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb && dpkg -i ./puppetlabs-release-pc1-xenial.deb
-        cd /tmp && wget http://apt.puppetlabs.com/pool/xenial/PC1/p/puppet-agent/puppet-agent_1.9.3-1xenial_amd64.deb && dpkg -i ./puppet-agent_1.9.3-1xenial_amd64.deb
+        cd /tmp && wget --quiet http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb && dpkg -i ./puppetlabs-release-pc1-xenial.deb
+        cd /tmp && wget --quiet http://apt.puppetlabs.com/pool/xenial/PC1/p/puppet-agent/puppet-agent_1.9.3-1xenial_amd64.deb && dpkg -i ./puppet-agent_1.9.3-1xenial_amd64.deb
       fi
