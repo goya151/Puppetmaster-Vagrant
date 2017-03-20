a2cc7b515b3bc26f28ff6daf959e2fdc26ca1b9f
Maksym Myrnyi <mmyrnyi@afdata.de>
2017-03-20 09:07:42 +0100

    Add puppetdb ssl-setup to fix ssl issue in puppetdb

--- roles/puppetmaster_role/manifests/init.pp
+++ roles/puppetmaster_role/manifests/init.pp
@@ -4,7 +4,13 @@
 
 class puppetmaster_role {
 
-  include puppetdb
+  class { 'puppetdb': } ->
+  exec { 'puppetdb-setup-ssl':
+    command => '/opt/puppetlabs/bin/puppetdb ssl-setup',
+    notify  => Service['puppetdb'],
+    unless  => '/usr/bin/test -d /etc/puppetlabs/puppetdb/ssl',
+    require => Package['puppetdb']
+  }
 
   define postgresql::globals {
     $default_version = '9.5'
