282cd1663712c8cfbe0e5341cf242568219d7525
Maksym Myrnyi <mmyrnyi@afdata.de>
2017-12-20 14:51:04 +0100

    Fix puppet agent installer

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,18 +8,18 @@ echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #      sudo echo "192.155.89.90 apt.puppetlabs.com" >> /etc/hosts
 #      sudo echo "217.196.149.50 postgresql.org" >> /etc/hosts
 
-      # Check puppet agent installed
+      AGENT_VERSION="5.3.3"
+
+      VERSION=`/usr/bin/puppet --version`
+
       if [ ! -f '/usr/bin/puppet' ]; then
         cd /tmp && wget https://apt.puppetlabs.com/puppet5-release-xenial.deb && apt-get install ./puppet5-release-xenial.deb
         apt-get -qq update
-        cd /tmp && wget https://apt.puppetlabs.com/pool/xenial/puppet5/p/puppet-agent/puppet-agent_5.0.1-1xenial_amd64.deb && apt-get install ./puppet-agent_5.0.1-1xenial_amd64.deb
+        cd /tmp && wget https://apt.puppetlabs.com/pool/xenial/puppet5/p/puppet-agent/puppet-agent_$AGENT_VERSION-1xenial_amd64.deb && apt-get install ./puppet-agent_$AGENT_VERSION-1xenial_amd64.deb
         ln -s /opt/puppetlabs/bin/puppet  /usr/bin/puppet
       fi
-
-      # Check current puppet version
-      VERSION=`/usr/bin/puppet --version`
       if [ ! $VERSION = "5.3.3" ]; then
         apt-get -y purge puppet*
         cd /tmp && wget https://apt.puppetlabs.com/puppet5-release-xenial.deb && apt-get install ./puppet5-release-xenial.deb
-        cd /tmp && wget https://apt.puppetlabs.com/pool/xenial/puppet5/p/puppet-agent/puppet-agent_$VERSION-1xenial_amd64.deb && apt-get install ./puppet-agent_$VERSION-1xenial_amd64.deb
+        cd /tmp && wget https://apt.puppetlabs.com/pool/xenial/puppet5/p/puppet-agent/puppet-agent_$AGENT_VERSION-1xenial_amd64.deb && apt-get install ./puppet-agent_$AGENT_VERSION-1xenial_amd64.deb
       fi
